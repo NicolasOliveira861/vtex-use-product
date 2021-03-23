eef584d9f4e000f87a0445b630ce418820802b76
Heraldo Ecard Roque Filho <me@heraldo.tech>
2021-03-23 18:25:04 -0300

    correçoes na build do checkout

--- checkout/webpack.common.js
+++ checkout/webpack.common.js
@@ -3,32 +3,25 @@ const path = require("path");
 var shopName = require("../storefront/package.json").shopName;
 /**
  * Configuração do webpack
- * @tutorial https://github.com/webpack/webpack.js.org/tree/v3.11.0/src/content
- * @tutorial https://medium.com/netscape/webpack-3-react-production-build-tips-d20507dba99a
  */
+
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
     entry: {
         checkout: "./src/arquivos/js/checkout.js",
     },
     output: {
-        path: path.resolve(__dirname, "/dist/arquivos"),
+        path: path.resolve(__dirname, "./dist/arquivos"),
         filename: shopName + "--[name]-bundle.js",
-        chunkLoading: false,
-        wasmLoading: false,
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"],
-                    },
-                },
-            },
-        ],
+    resolve: {
+        alias: {
+            Helpers: path.resolve(__dirname, "..", "src/arquivos/js/helpers"),
+            Lib: path.resolve(__dirname, "..", "src/arquivos/js/lib"),
+            Config: path.resolve(__dirname, "..", "src/arquivos/js/config"),
+            App: path.resolve(__dirname, "..", "src/arquivos/js/app"),
+        },
     },
     plugins: [
         new webpack.ProvidePlugin({
--- checkout/webpack.prod.js
+++ checkout/webpack.prod.js
@@ -2,22 +2,49 @@ const webpack = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
 
 module.exports = merge(common, {
-	externals: {
-		jquery: "jQuery",
-		vtexjs: "vtexjs",
-	},
-	mode: "production",
-	optimization: {
-		minimizer: [
-			new TerserPlugin({
-				extractComments: true,
-				terserOptions: {
-					keep_classnames: true,
-				},
-			}),
-		],
-	},
-	plugins: [new webpack.HashedModuleIdsPlugin()],
+    externals: {
+        jquery: "jQuery",
+        vtexjs: "vtexjs",
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                include: path.resolve(__dirname, "..", "src/arquivos/js"),
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["@babel/preset-env"], "@babel/react"],
+                        plugins: ["@babel/plugin-transform-async-to-generator"],
+                        cacheDirectory: true,
+                    },
+                },
+            },
+        ],
+    },
+    optimization: {
+        usedExports: true,
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: true,
+                terserOptions: {
+                    keep_classnames: true,
+                    keep_fnames: true,
+                    compress: {
+                        pure_funcs: [
+                            "console.table",
+                            "console.debug",
+                            "console.log",
+                        ],
+                    },
+                },
+            }),
+        ],
+    },
 });
